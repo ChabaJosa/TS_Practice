3d49c78cb4a180aca4940965b26aa5cb91b78fcd
ChabaJosa <chabagjg@gmail.com>
2022-05-10 10:49:09 -0400

    Added minor change

--- index.ts
+++ index.ts
@@ -3,5 +3,7 @@ import axios from "axios";
 const url = "https://jsonplaceholder.typicode.com/todos/1";
 //
 axios.get(url).then((response) => {
-  console.log(response.data);
+//   console.log(response.data);
+  const todo = response.data
+  console.log(`The user id is ${todo.userId}, the id is ${todo.id} for ${todo.title} and it ${todo.completed ? "has" :"has not"} been completed.`)
 });
