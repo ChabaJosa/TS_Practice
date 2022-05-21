9a9f271c6bf27879e2f4cc24550a6d52cd43a555
ChabaJosa <chabagjg@gmail.com>
2022-05-21 12:13:01 -0400

    Minor change

--- index.ts
+++ index.ts
@@ -2,8 +2,21 @@ import axios from "axios";
 //
 const url = "https://jsonplaceholder.typicode.com/todos/1";
 //
+interface TodoResponse {
+  userId: string;
+  id:number;
+  title:string;
+  completed: boolean;
+}
+//
 axios.get(url).then((response) => {
-//   console.log(response.data);
-  const todo = response.data
+  //
+  //    Our response has the following properties:
+  //        { id, userId, title, completed }
+  //
+  //    To make sure we have correct spelling and there's no 
+  //    undefined properties we will use an interface
+  //
+  const todo = response.data as TodoResponse
   console.log(`The user id is ${todo.userId}, the id is ${todo.id} for ${todo.title} and it ${todo.completed ? "has" :"has not"} been completed.`)
 });
